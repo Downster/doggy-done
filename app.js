e6a5843a7c89314ba30cead73abdaea6488555e8
Downster <b.downing.code@gmail.com>
2022-04-04 15:12:11 -0700

    Initial skeleton setup

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+const cookieParser = require("cookie-parser");
+const csrf = require("csurf");
+const csrfProtection = csrf({ cookie: true });
+
+const { HairColor, Person } = require("./models");
+
+//async error handler
+const asyncHandler = (handler) => (req, res, next) =>
+  handler(req, res, next).catch(next);
+
+//middleware goes here
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.set("view engine", "pug");
+//routes go here
+
+const port = 8081;
+app.listen(port, () => console.log(`Listening on port ${port}...`));
