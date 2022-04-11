48d9544bc4f65548f54767262765688bcfa44523
Downster <b.downing.code@gmail.com>
2022-04-11 15:49:04 -0700

    Added css files, logout page, navbar

--- db/models/contact.js
+++ db/models/contact.js
@@ -10,6 +10,7 @@ module.exports = (sequelize, DataTypes) => {
   );
 
 <<<<<<< HEAD
+<<<<<<< HEAD
   const columnMap = {
     through: "Contact",
     otherKey: "user_id1",
@@ -23,6 +24,21 @@ module.exports = (sequelize, DataTypes) => {
   Contact.associate = function(models) {
     // associations can be defined here
 >>>>>>> 8b3887d (update user self-association for contacts)
+=======
+  Contact.associate = function(models) {
+    // associations can be defined here
+=======
+  const columnMap = {
+    through: "Contact",
+    otherKey: "user_id1",
+    foreignKey: "user_id2",
+  };
+
+  Contact.associate = function (models) {
+    // associations can be defined here
+    // Contact.belongsToMany(models.Contact, columnMap)
+>>>>>>> 181fd02 (Added css files, logout page, navbar)
+>>>>>>> 023a344 (Added css files, logout page, navbar)
   };
   return Contact;
 };
