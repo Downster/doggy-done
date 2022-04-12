21a1c3bb3c5d652a1d538a6ce8dc2ec1af1d7756
Kai Seward <kairality@gmail.com>
2022-04-12 10:24:58 -0700

    api route for lists

--- routes/lists.js
+++ routes/lists.js
@@ -4,9 +4,21 @@ const db = require('../db/models');
 
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('splash-page', { title: 'Doggy Done!' });
-});
+/* GET lists */
+router.get('/', asyncHandler(async (req, res, next) => {
+    const { userId } = req.session.auth;
+    const lists = await db.List.findAll({where: userId, include: [{model: db.Task}]});
+    res.json(lists);
+}));
+
+router.post('/', asyncHandler(async (req, res, next) => {
+    const { userId } = req.session.auth;
+    const { listName } = req.body;
+    const list = await db.List.create({
+        owner_id: userId,
+        name: listName,
+    });
+    res.json(list);
+}));
 
 module.exports = router;
