590d785bb2b77f8518585fdd6c4340abf63f1298
Kai Seward <kairality@gmail.com>
2022-04-12 11:54:37 -0700

    finish migration for unique list name by owner ONLY

--- db/migrations/20220412174422-list_name_unique.js
+++ db/migrations/20220412174422-list_name_unique.js
@@ -10,15 +10,12 @@ module.exports = {
       return queryInterface.createTable('users', { id: Sequelize.INTEGER });
     */
       return Promise.all([
-        queryInterface.changeColumn("Lists", "name", {
-          type: Sequelize.STRING(35),
-          allowNull: false,
-        }),
         queryInterface.addConstraint('Lists', {
-          fields: ['owner_id', 'name'],
+          fields: ['name', 'owner_id'],
           type: 'unique',
           name: 'lists_unique_name_by_owner',
         }),
+        queryInterface.removeConstraint('Lists', 'Lists_name_key'),
       ])
 
   },
@@ -32,10 +29,10 @@ module.exports = {
       return queryInterface.dropTable('users');
     */
       return Promise.all([
-        queryInterface.changeColumn("Lists", "name", {
-          type: Sequelize.STRING(35),
-          allowNull: false,
-          unique: true,
+        queryInterface.addConstraint('Lists', {
+          fields: ['name'],
+          type: "unique",
+          name: "Lists_name_key"
         }),
         queryInterface.removeConstraint('Lists', 'lists_unique_name_by_owner'),
     ]);
