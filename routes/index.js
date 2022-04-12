7e263d4841295bfbf90141fac695869ccb211cba
Downster <b.downing.code@gmail.com>
2022-04-12 10:48:28 -0700

    App page is looking good!

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,24 @@
-var express = require('express');
-const { csrfProtection, asyncHandler } = require('./utils');
-const db = require('../db/models');
+var express = require("express");
+const { csrfProtection, asyncHandler } = require("./utils");
+const db = require("../db/models");
 
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-
-  res.render('splash-page', { title: 'Doggy Done!' });
+router.get("/", function (req, res, next) {
+  res.render("splash-page", { title: "Doggy Done!" });
   // if () {
   //   res.redirect('/register')
   // }
 });
+
+router.get(
+  "/shapoopi",
+  asyncHandler(async (req, res) => {
+    res.render("app", {
+      title: "Doggy-Done",
+    });
+  })
+);
 
 module.exports = router;
