4af108f00ebe1aa2fa7f2e2e179066ce8956c93a
Downster <b.downing.code@gmail.com>
2022-04-12 10:48:28 -0700

    App page is looking good!

--- routes/index.js
+++ routes/index.js
@@ -6,12 +6,15 @@ var router = express.Router();
 
 /* GET home page. */
 <<<<<<< HEAD
+<<<<<<< HEAD
 router.get("/", function (req, res, next) {
   res.render("splash-page", { title: "Doggy Done!" });
   // if () {
   //   res.redirect('/register')
   // }
 =======
+=======
+>>>>>>> baf9d35 (App page is looking good!)
 router.get('/', function(req, res, next) {
 
   res.render('splash-page', { title: 'Doggy Done!' });
@@ -33,6 +36,26 @@ router.get(
 router.get('/limsa-lominsa', function(req, res, next)  {
   res.render('limsa-lominsa', {title: 'Limsa Lominsa'})
 })
+<<<<<<< HEAD
 >>>>>>> 6ecb380 (add base routes for lists/dogs/notes and force email to lowercase)
+=======
+=======
+router.get("/", function (req, res, next) {
+  res.render("splash-page", { title: "Doggy Done!" });
+  // if () {
+  //   res.redirect('/register')
+  // }
+});
+
+router.get(
+  "/shapoopi",
+  asyncHandler(async (req, res) => {
+    res.render("app", {
+      title: "Doggy-Done",
+    });
+  })
+);
+>>>>>>> 7e263d4 (App page is looking good!)
+>>>>>>> baf9d35 (App page is looking good!)
 
 module.exports = router;
