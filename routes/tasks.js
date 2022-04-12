b673adbdd9eb12a734c616b4941cfa979631c74b
Downster <b.downing.code@gmail.com>
2022-04-12 16:30:49 -0700

    Flailing with selecting a task

--- routes/tasks.js
+++ routes/tasks.js
@@ -1,45 +1,60 @@
-const express = require('express');
-const db = require('../db/models');
-const { check, validationResult } = require('express-validator');
-const {csrfProtection, asyncHandler } = require('./utils');
+const express = require("express");
+const db = require("../db/models");
+const { check, validationResult } = require("express-validator");
+const { csrfProtection, asyncHandler } = require("./utils");
 const router = express.Router();
 
-router.get('/', asyncHandler(async(req,res) => {
+router.get(
+  "/",
+  asyncHandler(async (req, res) => {
     const tasks = await db.Task.findAll();
-    res.render('all-tasks', {title: 'Tasks 🐶', tasks})
+    res.render("all-tasks", { title: "Tasks 🐶", tasks });
+  })
+);
 
-}));
+router.get(
+  "/:id",
+  asyncHandler(async (req, res) => {
+    const taskId = req.params.id;
+    console.log(taskId);
+    const task = await db.Task.findByPk(taskId);
+    res.json({
+      message: "Success",
+      task,
+    });
+  })
+);
 
 const taskValidators = [
-    check('detail')
-        .exists({checkFalsy: true})
-        .withMessage('Task details are required'),
-    check('priority')
-        .exists({checkFalsy: true})
-        .withMessage('Must set task to complete or incomplete')
+  check("detail")
+    .exists({ checkFalsy: true })
+    .withMessage("Task details are required"),
+  check("priority")
+    .exists({ checkFalsy: true })
+    .withMessage("Must set task to complete or incomplete"),
 ];
 
-router.post('/add',csrfProtection,taskValidators,asyncHandler(async(req,res) => {
-    const {
-        detail,
-        due_date,
-        priority,
-        completed
-    } = req.body;
+router.post(
+  "/add",
+  csrfProtection,
+  taskValidators,
+  asyncHandler(async (req, res) => {
+    const { detail, due_date, priority, completed } = req.body;
 
     const task = db.Task.build({
-        detail,
-        due_date,
-        priority,
-        completed,
+      detail,
+      due_date,
+      priority,
+      completed,
     });
 
-    res.render('all-tasks', {
-        title: 'Add Tasks 🐶',
-        tasks,
-        errors,
-        csrfToken: req.csrfToken(),
-      });
-}));
+    res.render("all-tasks", {
+      title: "Add Tasks 🐶",
+      tasks,
+      errors,
+      csrfToken: req.csrfToken(),
+    });
+  })
+);
 
 module.exports = router;
