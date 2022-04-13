82a4227ef31328d1d08342112926b57195bbe9de
Downster <b.downing.code@gmail.com>
2022-04-13 10:54:24 -0700

    Got event listener working for create list thing

--- public/javascripts/closeListOnClick.js
+++ public/javascripts/closeListOnClick.js
@@ -1,12 +1,18 @@
-// document.body.addEventListener("click", async (e) => {
-//   const addListForm = document.querySelector(".add-list-form");
-//   if (addListForm.classList.contains("active")) {
-//     addListForm.classList.toggle("active");
-//     const appBody = document.querySelector(".app-body");
-//     const header = document.querySelector(".header-nav");
-//     const navMenu = document.querySelector(".nav-menu-container");
-//     appBody.classList.toggle("blur");
-//     header.classList.toggle("blur");
-//     navMenu.classList.toggle("blur");
-//   }
-// });
+const appBody = document.querySelector(".app-body");
+const header = document.querySelector(".header-nav");
+const navMenu = document.querySelector(".nav-menu-container");
+const listenerList = [appBody, header, navMenu];
+listenerList.forEach((ele) => {
+  ele.addEventListener("click", async (e) => {
+    const addListForm = document.querySelector(".add-list-form");
+    if (addListForm.classList.contains("active")) {
+      addListForm.classList.toggle("active");
+      const appBody = document.querySelector(".app-body");
+      const header = document.querySelector(".header-nav");
+      const navMenu = document.querySelector(".nav-menu-container");
+      appBody.classList.toggle("blur");
+      header.classList.toggle("blur");
+      navMenu.classList.toggle("blur");
+    }
+  });
+});
