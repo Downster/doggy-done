c247825606201d2ce734fba4aa9963d3ab1ff984
Kai Seward <kairality@gmail.com>
2022-04-13 12:53:04 -0700

    added utility function for fetching with csrf token

--- public/javascripts/completeTask.js
+++ public/javascripts/completeTask.js
@@ -1,19 +1,15 @@
-import {extractCSRFToken } from './utils.js';
+import { fetchWithToken } from './utils.js';
 
 const taskList = document.querySelectorAll(".task-checkbox");
 taskList.forEach((task) => {
   task.addEventListener("click", async (e) => {
     const id = e.target.id.split("-")[2];
-    const token = extractCSRFToken();
     const checked =
       document.querySelector(`#task-checkbox-${id}:checked`) !== null;
-    const res = await fetch(`/tasks/${id}/completed`, {
-      method: "PUT",
-      headers: { "Content-Type": "application/json", "CSRF-Token": token },
-      body: JSON.stringify({
+    const body = JSON.stringify({
         completed: checked,
-      }),
     });
+    const res = await fetchWithToken(`/tasks/${id}/completed`, 'POST', body);
     const data = await res.json();
     console.log(data);
   });
--- public/javascripts/utils.js
+++ public/javascripts/utils.js
@@ -4,3 +4,18 @@ export const extractCSRFToken = () => document.cookie
   .split(";")
   .find(token => token.includes("CSRF-Token"))
   .split("=")[1];
+
+  export const fetchWithToken = async (endpoint, method, body) => {
+      const token = extractCSRFToken();
+      const headers = {
+          "Content-Type": "application/json",
+          "CSRF-Token": token,
+      };
+      const requestParams = {
+          method,
+          credentials: "same-origin",
+          headers,
+          body,
+      };
+      return await fetch(endpoint, requestParams);
+  }
