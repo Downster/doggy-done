41724070f14b5e725034fbcc945dd8d1e1c5a517
Downster <b.downing.code@gmail.com>
2022-04-13 09:22:21 -0700

    Fixed demo user seeder

--- db/seeders/20220411221402-demoUser.js
+++ db/seeders/20220411221402-demoUser.js
@@ -1,30 +1,30 @@
 "use strict";
 const bcrypt = require("bcryptjs");
 const password = "Password123!";
-const hashed = await bcrypt.hash(password, 10);
 module.exports = {
   up: (queryInterface, Sequelize) => {
     /*
       Add altering commands here.
       Return a promise to correctly handle asynchronicity.*/
-
-    return queryInterface.bulkInsert(
-      "Users",
-      [
-        {
-          email: "joe@gmail.com",
-          display_name: "joeSchmoe",
-          firstName: "Joe",
-          lastName: "Schmoe",
-          password: hashed,
-          photo:
-            "https://www.looper.com/img/gallery/heres-where-you-can-watch-despicable-me/intro-1634355974.webp",
-          createdAt: new Date(),
-          updatedAt: new Date(),
-        },
-      ],
-      {}
-    );
+    return bcrypt.hash(password, 10).then((password) => {
+      return queryInterface.bulkInsert(
+        "Users",
+        [
+          {
+            email: "joe@gmail.com",
+            display_name: "joeSchmoe",
+            firstName: "Joe",
+            lastName: "Schmoe",
+            password: password,
+            photo:
+              "https://www.looper.com/img/gallery/heres-where-you-can-watch-despicable-me/intro-1634355974.webp",
+            createdAt: new Date(),
+            updatedAt: new Date(),
+          },
+        ],
+        {}
+      );
+    });
   },
 
   down: (queryInterface, Sequelize) => {
