121b76ff404b536623fd22c3d8d047d65d1b5b89
Kai Seward <kairality@gmail.com>
2022-04-14 11:07:14 -0700

    Dogs part 1

---
+++ public/javascripts/dogsLoad.js
@@ -0,0 +1,5 @@
+import * as DogUtils from "./dogUtils.js";
+
+document.addEventListener('DOMContentLoaded', async (_e) => {
+    await DogUtils.genDogsArea();
+})
--- public/javascripts/taskToggle.js
+++ public/javascripts/taskToggle.js
@@ -10,15 +10,18 @@ export const handleTaskToggler = async (e) => {
   appRight.classList.toggle("active");
   taskDetails.classList.toggle("active");
 
-  const res = await fetchWithToken(`/tasks/${id}`, "GET");
-  const data = await res.json();
-  const detailInput = taskDetails.getElementsByTagName("input");
-  const dueDate = taskDetails.getElementsByClassName("detail-date");
-  const taskId = document.getElementById("task-id");
-  taskId.innerText = id;
-  detailInput[0].value = data.task.detail;
-  //dueDate[0].value = data.task.due_date.split("T")[0];
-};
+        const res = await fetchWithToken(`/tasks/${id}`, "GET");
+        const {task} = await res.json();
+        const detailInput = taskDetails.getElementsByTagName("input");
+        const dueDate = taskDetails.getElementsByClassName("detail-date");
+        const taskId = document.getElementById('task-id');
+        taskId.innerText = id;
+        detailInput[0].value = data.task.detail;
+        if (data.task.task.dueDate) {
+          dueDate[0].value = data.task.due_date.split("T")[0];
+        }
+
+}
 
 export const handleCloseButton = (e) => {
   console.log("here");
