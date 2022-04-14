e6b9b84ac119ca520d97c22aab082c61acd9e76d
kairality <kairality@gmail.com>
2022-04-13 17:04:20 -0700

    Update demoUser.js

--- public/javascripts/demoUser.js
+++ public/javascripts/demoUser.js
@@ -4,10 +4,8 @@ const body = JSON.stringify({
     email: 'joe@gmail.com',
     password: 'Password123!'
 });
-// console.log('hello?');
 
 demoJoe.addEventListener('click', async(_e) => {
     const res = await fetchWithToken('users/login', 'POST', body);
-    // console.log('hello??');
     window.location.href = '/app';
 });
