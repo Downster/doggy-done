cb319493a838839f6be19163ae29cb2c057d866a
Maica <maicasantos90@gmail.com>
2022-04-14 10:24:07 -0700

    signout complete

--- public/javascripts/sign-out.js
+++ public/javascripts/sign-out.js
@@ -1,11 +1,24 @@
-import { fetchWithToken } = from('/utils.js');
+import { fetchWithToken } from './utils.js';
+
+// console.log(logoutUser);
+
+
 
 const signOutBtn = document.getElementById('sign-out-btn');
 
-console.log(signOutBtn);
-
-signOutBtn.addEventListener()
 
 signOutBtn.addEventListener('click', async(e) => {
-    console.log("CLICKED!!! ")
+    console.log('log me out please');
+    // console.log(req.session.auth)
+    // btn is clicked
+        // cookie
+        // session
+            // user is logged out of app
+        //redirect to home
+     await fetch("/users/logout", {
+         method: "POST",
+         headers: { "Content-Type": "text/html" }})
+
 })
+
+//might need to change to app .json
