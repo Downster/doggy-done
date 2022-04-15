6ca7b016c86ac180c5fb881ddd70960f89e56b9a
Downster <b.downing.code@gmail.com>
2022-04-15 11:49:05 -0700

    searching is getting close

--- public/javascripts/searchBar.js
+++ public/javascripts/searchBar.js
@@ -1,11 +1,24 @@
+import { fetchWithToken } from "./utils.js";
+
 const searchBar = document.getElementById("search");
 
-searchBar.addEventListener("click", (e) => {
-  console.log("clicked bar");
-});
+const generateSearchArea = async () => {
+  const searchDiv = document.createElement("div");
+  searchDiv.id = "search-div";
+  console.log("generated");
+};
+
+searchBar.addEventListener("click", generateSearchArea);
 
 searchBar.addEventListener("keyup", (e) => {
+  const currTasks = [];
+  const allTasks = document.querySelectorAll(".task-click");
   console.log(e.target.value);
+  allTasks.forEach((el) => {
+    currTasks.push(el.innerText.toLowerCase());
+  });
+  const filteredTasks = currTasks.filter((task) =>
+    task.includes(e.target.value)
+  );
+  console.log(filteredTasks);
 });
-
-export const genSearchArea = async () => {};
