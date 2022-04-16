941085a87b5f8482295918ee7e4a12ba194a1f70
Downster <b.downing.code@gmail.com>
2022-04-16 10:50:42 -0700

    Lists are fixed

--- public/javascripts/createList.js
+++ public/javascripts/createList.js
@@ -1,44 +1,51 @@
 import { fetchWithToken } from "./utils.js";
 import { populateTasksAndAddListeners } from "./taskUtils.js";
+import { populateLists} from "./listUtils.js";
 
-const addListButton = document.querySelector(".add-list");
+export const createListListener = (e) => {
 
-const listFormButton = document.getElementById("create-list");
+    const addListButton = document.querySelector(".add-list");
 
-const handleListAdd = async (e) => {
-  e.preventDefault();
-  const addListForm = document.querySelector(".add-list-form");
-  const appBody = document.querySelector(".app-body");
-  const header = document.querySelector(".header-nav");
-  const navMenu = document.querySelector(".nav-menu-container");
-  const input = document.getElementById("list-name-input");
-  const listName = input.value;
-  const body = JSON.stringify({listName});
-  try {
-    const response = await fetchWithToken("/lists", "POST", body);
-    const { list } = await response.json();
-    const newListId = list.id;
-    addListForm.classList.toggle("active");
-    appBody.classList.toggle("blur");
-    header.classList.toggle("blur");
-    navMenu.classList.toggle("blur");
-    await populateTasksAndAddListeners("list", newListId);
-  } catch(e) {
-    alert("Couldn't add list! Do you already have a list with that name?")
-  }
+    const listFormButton = document.getElementById("create-list");
 
+    const handleListAdd = async (e) => {
+        e.preventDefault();
+        const addListForm = document.querySelector(".add-list-form");
+        const appBody = document.querySelector(".app-body");
+        const header = document.querySelector(".header-nav");
+        const navMenu = document.querySelector(".nav-menu-container");
+        const input = document.getElementById("list-name-input");
+        const listName = input.value;
+        const body = JSON.stringify({ listName });
+        try {
+            const response = await fetchWithToken("/lists", "POST", body);
+            const { list } = await response.json();
+            const newListId = list.id;
+            addListForm.classList.toggle("active");
+            appBody.classList.toggle("blur");
+            header.classList.toggle("blur");
+            navMenu.classList.toggle("blur");
+            await populateTasksAndAddListeners("list", newListId);
+            await populateLists();
+            input.value = ''
+        } catch (e) {
+            alert("Couldn't add list! Do you already have a list with that name?")
+        }
+
+    }
+
+    listFormButton.addEventListener("click", handleListAdd);
+
+    addListButton.addEventListener("click", async (e) => {
+        console.log('clicky')
+        e.stopPropagation();
+        const addListForm = document.querySelector(".add-list-form");
+        const appBody = document.querySelector(".app-body");
+        const header = document.querySelector(".header-nav");
+        const navMenu = document.querySelector(".nav-menu-container");
+        addListForm.classList.toggle("active");
+        appBody.classList.toggle("blur");
+        header.classList.toggle("blur");
+        navMenu.classList.toggle("blur");
+    });
 }
-
-listFormButton.addEventListener("click", handleListAdd);
-
-addListButton.addEventListener("click", async (e) => {
-  e.stopPropagation();
-  const addListForm = document.querySelector(".add-list-form");
-  const appBody = document.querySelector(".app-body");
-  const header = document.querySelector(".header-nav");
-  const navMenu = document.querySelector(".nav-menu-container");
-  addListForm.classList.toggle("active");
-  appBody.classList.toggle("blur");
-  header.classList.toggle("blur");
-  navMenu.classList.toggle("blur");
-});
