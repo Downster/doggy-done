13c1dfe99e46158635c983c7c82cc9156d1ef088
Downster <b.downing.code@gmail.com>
2022-04-17 18:22:34 -0700

    Fixed empty task

--- public/javascripts/addTask.js
+++ public/javascripts/addTask.js
@@ -10,26 +10,30 @@ dateInput.valueAsDate = new Date();
 const handleAddTask = async (e) => {
   const input = document.getElementById("task-input");
   const priority = document.getElementById("task-priority");
-  const date = document.getElementById("task-date");
-  const body = JSON.stringify({
-    detail: input.value,
-    due_date: dayjs(date.value),
-    priority: priority.value,
-    completed: false,
-  });
-  if (document.querySelector(".edit-list-button")) {
-    const listId = document
-      .querySelector(".edit-list-button")
-      .id.split("edit-list-")
-      .pop();
-    const res = await fetchWithToken(`/lists/${listId}/tasks`, "POST", body);
-    await populateTasksAndAddListeners("list", listId);
-  } else {
-    const res = await fetchWithToken("/tasks", "POST", body);
-    await populateTasksAndAddListeners();
-  }
-  input.value = null;
-  priority.value = 1;
+    const date = document.getElementById("task-date");
+    if (input.value === '') {
+        window.alert('Please input a non empty value')
+    } else {
+        const body = JSON.stringify({
+            detail: input.value,
+            due_date: dayjs(date.value),
+            priority: priority.value,
+            completed: false,
+        });
+        if (document.querySelector(".edit-list-button")) {
+            const listId = document
+                .querySelector(".edit-list-button")
+                .id.split("edit-list-")
+                .pop();
+            const res = await fetchWithToken(`/lists/${listId}/tasks`, "POST", body);
+            await populateTasksAndAddListeners("list", listId);
+        } else {
+            const res = await fetchWithToken("/tasks", "POST", body);
+            await populateTasksAndAddListeners();
+        }
+        input.value = null;
+        priority.value = 1;
+    }
 }
 
 const handleAddTaskWithKeydownWrapper = async (e) => {
